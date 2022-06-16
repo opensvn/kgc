96994671876aac143b82ce133c21a0b2260a4889
opensvn <justleefeng@gmail.com>
2022-06-16 22:48:50 +0800

    add master key members

--- kgc.go
+++ kgc.go
@@ -1,9 +1,29 @@
 package kgc
 
-type Kgc struct {
+import (
+	"crypto/rand"
 
+	"github.com/emmansun/gmsm/sm9"
+)
+
+type Kgc struct {
+	encryptMasterKey *sm9.EncryptMasterPrivateKey
+	signMasterKey    *sm9.SignMasterPrivateKey
 }
 
 func New() *Kgc {
-	return &Kgc{}
+	encryptMasterKey, err := sm9.GenerateEncryptMasterKey(rand.Reader)
+	if err != nil {
+		return nil
+	}
+
+	signMasterKey, err := sm9.GenerateSignMasterKey(rand.Reader)
+	if err != nil {
+		return nil
+	}
+
+	return &Kgc{
+		encryptMasterKey: encryptMasterKey,
+		signMasterKey:    signMasterKey,
+	}
 }
--- kgc_test.go
+++ kgc_test.go
@@ -7,4 +7,12 @@ func TestNewKgc(t *testing.T) {
 	if kgc == nil {
 		t.Error("kgc is nil")
 	}
+
+	if kgc.encryptMasterKey == nil {
+		t.Error("kgc.encryptMasterKey is nil")
+	}
+
+	if kgc.signMasterKey == nil {
+		t.Error("kgc.signMasterKey is nil")
+	}
 }
